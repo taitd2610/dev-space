5ff4ed01ae7bc927f76785a98051d8b55a21951f
taitd153 <trinhdinhtai1996@gmail.com>
2021-09-21 15:48:32 +0700

    update Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = ({ icon, title, borderColor, setTop }) => {
   return (
     <div
-      className={`sticky z-10 top-0 flex flex-row justify-center font-semibold p-3 border-t-8 bg-white ${borderColor}`}
+      className={`sticky z-10 top-0 flex flex-row justify-center font-semibold p-3 border-t-8 bg-white ${borderColor} shadow-md`}
     >
       <div className="ml-1 mr-auto flex flex-row">
         <img src={icon} alt="logo" className="h-6 w-6 m-1 mr-2" />
